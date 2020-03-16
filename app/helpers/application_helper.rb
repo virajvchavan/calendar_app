0a76acbe7e1fa015270d5c74ddd9ca6445f054a0
Viraj Chavan <virajc@live.com>
2020-03-16 10:16:01 +0530

    Show Event date in view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def format_event_duration(event, action)
+    if action == 'daily' && event['all_day_event']
+      return "<p class='chip'>All day</p>".html_safe
+    end
+
+    format = event['all_day_event'] ? '%d %b %Y' : '%d %b %Y %H:%M UTC'
+
+    html = "<p>"
+    html += "<div class='chip'>#{get_time(event['start_time'],format)} </div> - "
+    html += "<div class='chip'>#{get_time(event['end_time'],format)}</div>"
+    html += "</p>"
+    html.html_safe
+  end
+
+  def get_time(datetime_string, format)
+    DateTime.parse(datetime_string).strftime(format)
+  end
 end
